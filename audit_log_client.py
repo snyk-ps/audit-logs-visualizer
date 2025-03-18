dcc91539088125c3179b56eeae7a7cd0ce430ece
luciano-mori <luciano.mori@snyk.io>
2025-03-17 22:29:22 -0400

    add support for query group and org

--- audit_log_client.py
+++ audit_log_client.py
@@ -5,27 +5,23 @@ import os
 from datetime import datetime, timedelta
 import logging
 from dotenv import load_dotenv
-from utils import check_date_format 
+from utils import check_date_format
 
 logger = logging.getLogger(__name__)
 
-# Define the default maximum number of pages and default number of days
-DEFAULT_MAX_PAGES = 110
-DEFAULT_DAYS = 2
-
+DEFAULT_MAX_PAGES = 100
+DEFAULT_DAYS = 7
 
 class AuditLogClient:
-    def __init__(self, base_url: str, api_key: str, org_id: str):
+    def __init__(self, base_url: str, api_key: str):
         self.base_url = base_url
         self.api_key = api_key
-        self.org_id = org_id
         self.headers = {
             "Authorization": f"token {self.api_key}",
             "Content-Type": "application/json",
         }
 
     def _get_dates(self, from_date: Optional[str] = None, to_date: Optional[str] = None) -> tuple[str, str]:
-        """Helper function to get from_date and to_date."""
         if from_date is not None and to_date is not None:
             if not check_date_format(from_date):
                 raise ValueError(f"Incorrect from_date format. Use YYYY-MM-DDTHH:MM:SSZ. Input: {from_date}")
@@ -40,31 +36,37 @@ class AuditLogClient:
             from_date = (today - timedelta(days=DEFAULT_DAYS)).strftime("%Y-%m-%dT%H:%M:%SZ")
             return from_date, to_date
 
-
     def query_audit_logs(
         self,
-        group_id: str,
+        query_type: str,
+        query_id: str,
         event: Optional[str] = None,
         user_id: Optional[str] = None,
         page_size: Optional[int] = None,
         page: Optional[int] = None,
         from_date: Optional[str] = None,
         to_date: Optional[str] = None,
-        debug: bool = False,
+        debug: bool = False, #######################################################################################################################
     ) -> Dict[str, Any]:
-        url = f"{self.base_url}/groups/{group_id}/audit_logs/search?version=2024-10-15"
-        params = {}
 
+        if query_type == "org":
+            url = f"{self.base_url}/orgs/{query_id}/audit_logs/search?version=2024-10-15"
+        elif query_type == "group":
+            url = f"{self.base_url}/groups/{query_id}/audit_logs/search?version=2024-10-15"
+        else:
+            raise ValueError("Invalid query_type. Must be 'org' or 'group'.")
+
+        params = {}
         if event:
             params["filter[event]"] = event
         if user_id:
             params["filter[userId]"] = user_id
 
         try:
-          from_date, to_date = self._get_dates(from_date, to_date)
+            from_date, to_date = self._get_dates(from_date, to_date)
         except ValueError as e:
-          logger.error(e)
-          return {"logs": [], "has_more": False, "total": 0}
+            logger.error(e)
+            return {"logs": [], "has_more": False, "total": 0}
 
         params["from"] = from_date
         params["to"] = to_date
@@ -75,6 +77,7 @@ class AuditLogClient:
             params["page[number]"] = page
 
         if debug:
+            logger.setLevel(logging.DEBUG)
             logger.debug(f"Querying URL: {url}")
             logger.debug(f"Request Headers: {self.headers}")
             logger.debug(f"Query Parameters: {params}")
@@ -86,11 +89,11 @@ class AuditLogClient:
 
             if "data" in data and "items" in data["data"]:
                 if debug:
-                    logger.debug(f"Full Response: {json.dumps(data, indent=2)}")
+                    ##logger.debug(f"Full Response: {json.dumps(data, indent=2)}")
                 return {
                     "logs": data["data"]["items"],
                     "has_more": data.get("links", {}).get("next") is not None,
-                    "total": data["data"].get("meta", {}).get("total")
+                    "total": data["data"].get("meta", {}).get("total"),
                 }
             else:
                 logger.error(
@@ -104,9 +107,11 @@ class AuditLogClient:
             logger.error(f"Error decoding JSON response: {e}")
             return {"logs": [], "has_more": False, "total": 0}
 
+
     def get_all_audit_logs(
         self,
-        group_id: str,
+        query_type: str,
+        query_id: str,
         event: Optional[str] = None,
         user_id: Optional[str] = None,
         page_size: Optional[int] = 100,
@@ -122,7 +127,8 @@ class AuditLogClient:
         while has_more and page <= max_pages:
             logger.info(f"Retrieving page {page} of audit logs")
             result = self.query_audit_logs(
-                group_id=group_id,
+                query_type=query_type,
+                query_id=query_id,
                 event=event,
                 user_id=user_id,
                 page_size=page_size,
