6b021d3a307c20be7cd83a64582c051aa336545d
luciano-mori <luciano.mori@snyk.io>
2025-04-03 10:16:34 -0400

    adding front end

---
+++ event-visualizer/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CSVEventVisualizer from './CSVEventVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <CSVEventVisualizer />
+    </div>
+  );
+}
+
+export default App;
