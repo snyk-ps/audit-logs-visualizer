d79a0ea94038f1462fc262b3b8c3117e9d133b32
luciano-mori <luciano.mori@snyk.io>
2025-04-03 18:11:47 -0400

    Add unit tests for CSVEventVisualizer component and utility functions

--- event-visualizer/src/App.test.js
+++ event-visualizer/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Event Data Visualizer', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Event Data Visualizer/i);
+  expect(headingElement).toBeInTheDocument();
 });
